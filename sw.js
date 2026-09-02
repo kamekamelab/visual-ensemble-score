b585e17a7bb9f0c8b2dab6c15929f9ec999766f3
kamekamelab <318813420+kamekamelab@users.noreply.github.com>
2026-09-02 23:24:23 +0900

    Bump v4 cache after final UI cleanup

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='digital-score-v4.0.1';
+const CACHE='digital-score-v4.0.2';
 const ASSETS=['./','./index.html','./styles-v4.css','./app-v4.js','./editor-v4.js','./manifest.webmanifest'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
